da6eca418a47790fcc7ef7ea98a42973424805cb
rockem <eli.segal@gmail.com>
2018-02-18 08:22:25 +0200

    Java initial commit

---
+++ src/main/java/org/triberg/broadcast/Application.java
@@ -0,0 +1,11 @@
+package org.triberg.broadcast;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
