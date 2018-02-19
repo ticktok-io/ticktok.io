182a6db5a67aafdae2bfc124106742160941233f
rockem <eli.segal@gmail.com>
2018-02-19 23:33:19 +0200

    in progress

---
+++ src/test/java/e2e/org/triberg/broadcast/ApplicationE2ETest.java
@@ -0,0 +1,19 @@
+package e2e.org.triberg.broadcast;
+
+import org.junit.Test;
+
+import static java.lang.Thread.sleep;
+
+public class ApplicationE2ETest {
+
+    private static final long SECOND = 1000;
+
+    private final TicktokClient client = new TicktokClient();
+
+    @Test
+    public void delegateScheduledMessageToQueue() throws Exception {
+        client.registeredFor("once.in.1.second");
+        sleep(SECOND);
+        client.receivedClock();
+    }
+}
---
+++ src/test/java/e2e/org/triberg/broadcast/TicktokClient.java
@@ -0,0 +1,44 @@
+package e2e.org.triberg.broadcast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.JSONObject;
+import org.junit.Assert;
+
+import java.io.IOException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TicktokClient {
+
+    private static final String CLIENT_ID = "e2e-client";
+
+    private final CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    public void registeredFor(String timeExpr) throws IOException {
+        HttpResponse response = Request.Post("http://localhost:8080/api/v1/clocks")
+                .bodyString(createClockRequestFor(timeExpr), ContentType.APPLICATION_JSON)
+                .execute().returnResponse();
+        assertThat(response.getStatusLine().getStatusCode(), is(HttpStatus.SC_CREATED));
+        
+    }
+
+    private String createClockRequestFor(String timeExpr) {
+        return new JSONObject()
+                .put("schedule", timeExpr)
+                .put("clientId", CLIENT_ID)
+                .toString();
+    }
+
+    public void receivedClock() {
+
+    }
+}
