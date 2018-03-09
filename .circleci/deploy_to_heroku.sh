df74c4d480b1e70dce142f93fcb64265ead2fae9
rockem <eli.segal@gmail.com>
2018-03-09 19:21:26 +0200

    Refactor circleci

--- .circleci/deploy_to_heroku.sh
+++ .circleci/deploy_to_heroku.sh
@@ -2,13 +2,17 @@
 
 set -e
 
+HEROKU=registry.heroku.com
+
 if [ ${CIRCLE_BRANCH} == "master" ] || [ ${CIRCLE_BRANCH} == "develop" ]; then
     if [ "$CIRCLE_BRANCH" == "master" ]; then
         export TAG=$(git describe --tags --abbrev=0)
+        IMAGE=$HEROKU/ticktok-io-demo:$TAG
     else
         export TAG=$(git rev-parse --short HEAD)
+        IMAGE=$HEROKU/ticktok-io-dev:$TAG
     fi
-    docker tag app $IMAGE:$TAG
-    echo $HEROKU_PASSWORD | docker login -u $HEROKU_USERNAME --password-stdin registry.heroku.com
-    docker push registry.heroku.com/$IMAGE:$TAG/web
+    heroku container:login
+    docker tag app $IMAGE
+    docker push $IMAGE/web
 fi
---
+++ .circleci/publish_to_dockerhub.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
+    TAG=`git describe --tags --abbrev=0`
+    IMAGE=ticktok/ticktok:$TAG
+    if [[ `docker pull $IMAGE` ]]; then
+        echo $IMAGE already exists
+        exit 1
+    else
+        echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
+        docker tag app $IMAGE
+        docker push $IMAGE
+    fi
+fi
