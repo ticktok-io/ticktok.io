2484a9643df3fcd4accceb29af81975b1476bd8c
rockem <eli.segal@gmail.com>
2018-03-09 16:36:05 +0200

    Fixed circleci script

--- .circleci/upload_to_heroku.sh
+++ .circleci/upload_to_heroku.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo ${CIRCLE_BRANCH}
+
 if [ ${CIRCLE_BRANCH} == "master" ] || [ ${CIRCLE_BRANCH} == "develop" ]; then
     if [ "$CIRCLE_BRANCH" == "master" ]; then
         export TAG=$(git describe --tags --abbrev=0)
