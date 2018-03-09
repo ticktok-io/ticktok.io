326b57b8011f7136294c3b402da2c1960ac7e30f
rockem <eli.segal@gmail.com>
2018-03-09 15:15:00 +0200

    Fixed circleci script

--- .circleci/upload_to_heroku.sh
+++ .circleci/upload_to_heroku.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "${CIRCLE_BRANCH}" == "master" ] || [ "${CIRCLE_BRANCH}" == "develop" ]; then
     if [ "${CIRCLE_BRANCH}" == "master" ]; then
-        export TAG=`git describe --tags --abbrev=0`
+        export TAG=$(git describe --tags --abbrev=0)
     else
-        export TAG=`git rev-parse --short HEAD`
+        export TAG=$(git rev-parse --short HEAD)
     fi
     docker tag app $IMAGE:$TAG
     docker login --username=$HEROKU_USERNAME --password=$HEROKU_PASSWORD registry.heroku.com
