bf8cf756c52f31592a313388ca7acceb177dd33a
rockem <eli.segal@gmail.com>
2018-03-09 20:32:20 +0200

    Fixed circleci script

--- .circleci/deploy_to_heroku.sh
+++ .circleci/deploy_to_heroku.sh
@@ -7,13 +7,13 @@ HEROKU=registry.heroku.com
 if [ ${CIRCLE_BRANCH} == "master" ] || [ ${CIRCLE_BRANCH} == "develop" ]; then
     if [ "$CIRCLE_BRANCH" == "master" ]; then
         export TAG=$(git describe --tags --abbrev=0)
-        IMAGE=$HEROKU/ticktok-io-demo:$TAG
+        IMAGE=$HEROKU/ticktok-io-demo/web:$TAG
     else
         export TAG=$(git rev-parse --short HEAD)
-        IMAGE=$HEROKU/ticktok-io-dev:$TAG
+        IMAGE=$HEROKU/ticktok-io-dev/web:$TAG
     fi
     docker login --username=_ --password=$(heroku auth:token) $HEROKU
     echo image: $IMAGE
-    docker tag app $IMAGE/web
-    docker push $IMAGE/web
+    docker tag app $IMAGE
+    docker push $IMAGE
 fi
