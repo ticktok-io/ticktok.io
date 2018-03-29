59ae7c9718e13b14dba844013a33462e1640d83d
rockem <eli.segal@gmail.com>
2018-03-29 08:32:10 +0300

    timeout test

--- .circleci/deploy_to_heroku.sh
+++ .circleci/deploy_to_heroku.sh
@@ -9,7 +9,7 @@ check_health() {
     until [[ $(curl --silent  --fail $1/mgmt/health/ 2>&1 | grep '"UP"') != "" ]]; do
         sleep 1
     done
-    echo ticktok.io is healthy!
+    echo $1 is healthy!
 }
 
 
