2e365763bccf3d26e0ec08d3816b45b709f6eabb
rockem <eli.segal@gmail.com>
2018-09-24 21:10:15 +0300

    Fixed dummy test

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -10,7 +10,7 @@ class App extends Component {
           <nav className="navbar navbar-dark bg-dark">
             <h4>Ticktok.io playground</h4>
           </nav>
-          <h1 className="greeting" id="greeting">Hello Worlsss</h1>
+          <h1 className="greeting" id="greeting">Hello World</h1>
         </div>
       </div>
     );
