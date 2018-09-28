66aa342bc325786fb3963e689b04b5de068e03a2
rockem <eli.segal@gmail.com>
2018-09-28 15:40:51 +0300

    small fix

--- src/main/java/io/ticktok/server/config/DashboardWebConfig.java
+++ src/main/java/io/ticktok/server/config/DashboardWebConfig.java
@@ -1,22 +1,12 @@
 package io.ticktok.server.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.util.Assert;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class DashboardWebConfig implements WebMvcConfigurer {
-
-    @Autowired
-    private Environment env;
-
-    @Value("${resources.projectroot}")
-    private String projectRoot;
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -25,19 +15,8 @@ public class DashboardWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:public/");
 
-        boolean devMode = this.env.acceptsProfiles("development");
-
-        String location = devMode ? "file:///" + getProjectRootRequired()  + "/src/main/resources/public/" :"classpath:public/";
-        Integer cachePeriod = devMode ? 0 : null;
-
-        registry.addResourceHandler("/**")
-                .addResourceLocations(location)
-                .setCachePeriod(cachePeriod);
     }
 
-    private String getProjectRootRequired() {
-        Assert.state(this.projectRoot != null, "Please set \"resources.projectRoot\" in application.yml");
-        return this.projectRoot;
-    }
 }
