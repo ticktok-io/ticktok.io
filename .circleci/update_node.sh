0212a953ad7fa1b2ced9acdb01aa3a42d2ee19de
Eli Segal <eli.segal@gmail.com>
2018-12-19 07:49:12 +0200

    fix ci

--- .circleci/update_node.sh
+++ .circleci/update_node.sh
@@ -3,8 +3,8 @@
 # Using Ubuntu
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
-systemctl stop apt-daily.service
-systemctl kill --kill-who=all apt-daily.service
+sudo systemctl stop apt-daily.service
+sudo systemctl kill --kill-who=all apt-daily.service
 
 # wait until `apt-get updated` has been killed
 while ! (systemctl list-units --all apt-daily.service | fgrep -q dead)
