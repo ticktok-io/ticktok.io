df8fbf1966226347006b5f306ecf7d004b2b08eb
Eli Segal <eli.segal@gmail.com>
2018-12-19 07:46:37 +0200

    fix ci

--- .circleci/update_node.sh
+++ .circleci/update_node.sh
@@ -2,4 +2,14 @@
 
 # Using Ubuntu
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+systemctl stop apt-daily.service
+systemctl kill --kill-who=all apt-daily.service
+
+# wait until `apt-get updated` has been killed
+while ! (systemctl list-units --all apt-daily.service | fgrep -q dead)
+do
+  sleep 1;
+done
+
 sudo apt-get install -y nodejs
