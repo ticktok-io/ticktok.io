9fc8cf0d47ab66bfaed9b5abb47778ec242bac17
Eli Segal <eli.segal@gmail.com>
2018-12-21 16:09:53 +0200

    fix ci

--- docker-e2e/start_app.sh
+++ docker-e2e/start_app.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 docker run \
-    - RABBIT_URI=$RABBIT_URI
-    - MONGO_URI=$MONGO_URI
-    - SELF_DOMAIN=http://localhost:8081
-    - ACCESS_TOKEN=$E2E_ACCESS_TOKEN
+    -e RABBIT_URI=$RABBIT_URI
+    -e MONGO_URI=$MONGO_URI
+    -e SELF_DOMAIN=http://localhost:8081
+    -e ACCESS_TOKEN=$E2E_ACCESS_TOKEN
     -p 8081:8080 \
     -v ./application.yml:/opt/app/config/application.yml \
     -d \
