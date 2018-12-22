877b0518a3cb423b357bda3f97a5fd29cb9038bc
Eli Segal <eli.segal@gmail.com>
2018-12-22 22:33:52 +0200

    fix ci

--- .circleci/run_npm.sh
+++ .circleci/run_npm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-docker run -v `pwd`:/opt/dashboard -w /opt/dashboard node:8-alpine npm "$@"
+docker run -v `pwd`/dashboard:/opt/dashboard -w /opt/dashboard node:8-alpine npm "$@"
