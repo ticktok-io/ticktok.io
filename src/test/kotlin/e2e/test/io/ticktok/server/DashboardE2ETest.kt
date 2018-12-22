7567c90f0e160172d5161276867aaf9ab602428d
Eli Segal <eli.segal@gmail.com>
2018-12-22 17:08:25 +0200

    fix ci

--- src/test/kotlin/e2e/test/io/ticktok/server/DashboardE2ETest.kt
+++ src/test/kotlin/e2e/test/io/ticktok/server/DashboardE2ETest.kt
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty
+import java.lang.Thread.sleep
 
 @DisabledIfSystemProperty(named = "scope", matches = "server")
 @TestInstance(PER_CLASS)
@@ -19,6 +20,7 @@ class DashboardE2ETest : AppE2ETest() {
     @BeforeAll
     override fun startApp() {
         super.startApp()
+        sleep(5000)
         browser.start()
     }
 
