2f3d68f0e3bc86688eeb1b223e2a741e92a1ee35
Eli Segal <eli.segal@gmail.com>
2019-01-18 16:55:58 +0200

    updated docker startup file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,5 @@
 
 port=${PORT:-8080}
 
-if [[ ! -v MONGO_URI ]]; then
-    mongod --storageEngine inMemory --dbpath /tmp/mongo &
-fi
-exec mongod java -Dserver.port=$port -Xmx32m -Xss256k -Djava.security.egd=file:/dev/./urandom -jar /opt/app/app.jar
+[[ -z "$MONGO_URI" ]] && { export MONGO_URI=mongodb://localhost/ticktok; mkdir /tmp/mongo; mongod --storageEngine ephemeralForTest --dbpath /tmp/mongo & }
+exec java -Dserver.port=$port -Xmx32m -Xss256k -Djava.security.egd=file:/dev/./urandom -jar /opt/app/app.jar
