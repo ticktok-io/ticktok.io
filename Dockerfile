2bf26f8dcf9f942d074a5915a49f97f9f3c8d1aa
Eli Segal <eli.segal@gmail.com>
2019-01-18 16:22:33 +0200

    Trying CI with internal mongo

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,13 @@ FROM openjdk:8-jdk-alpine
 
 VOLUME /tmp
 
+RUN apk add mongodb
+
 ADD build/libs/ticktok-*.jar /opt/app/app.jar
 ADD entrypoint.sh /opt/app
 RUN chmod +x /opt/app/entrypoint.sh
+
+
 
 WORKDIR /opt/app
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,7 @@
 
 port=${PORT:-8080}
 
-exec java -Dserver.port=$port -Xmx32m -Xss256k -Djava.security.egd=file:/dev/./urandom -jar /opt/app/app.jar
+if [[ ! -v MONGO_URI ]]; then
+    mongod --storageEngine inMemory --dbpath /tmp/mongo &
+fi
+exec mongod java -Dserver.port=$port -Xmx32m -Xss256k -Djava.security.egd=file:/dev/./urandom -jar /opt/app/app.jar
