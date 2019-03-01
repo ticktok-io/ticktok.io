36ae30d0edf484349eda4c5a45cfadcce41df93b
Eli Segal <eli.segal@gmail.com>
2019-03-01 22:08:13 +0200

    choose http profile when no rabbit uri is given

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,7 @@ if [[ -z "${MONGO_URI}" ]]; then
     mkdir /tmp/mongo
     mongod --storageEngine ephemeralForTest --dbpath /tmp/mongo &
 fi
+if [[ -z "${RABBIT_URI}" ]]; then
+    export SPRING_PROFILES_ACTIVE=http
+fi
 exec java -Dserver.port=$port -Xmx64m -Xss512k -Djava.security.egd=file:/dev/./urandom -jar /opt/app/app.jar
