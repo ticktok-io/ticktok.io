f721a8d330b40dffc779e8983cee4348b6dc0dc1
Eli Segal <eli.segal@gmail.com>
2019-03-02 11:42:17 +0200

    remove redundant code

--- src/main/java/io/ticktok/server/Application.java
+++ src/main/java/io/ticktok/server/Application.java
@@ -7,20 +7,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class Application {
 
-    private static ConfigurableApplicationContext context;
-
     public static void main(String... args) {
-        context = SpringApplication.run(Application.class, args);
-    }
-
-    public static void restart(String... args) {
-        Thread thread = new Thread(() -> {
-            context.close();
-            System.gc();
-            context = SpringApplication.run(Application.class, args);
-        });
-        thread.setDaemon(false);
-        thread.start();
+        SpringApplication.run(Application.class, args);
     }
 
 }
