6d705d83ebee089130b20675e8888919c94203b3
Eli Segal <eli.segal@gmail.com>
2019-08-03 18:05:03 +0300

    implemented

--- src/main/java/io/ticktok/server/clock/actions/RepositoryClockAction.java
+++ src/main/java/io/ticktok/server/clock/actions/RepositoryClockAction.java
@@ -1,7 +1,7 @@
 package io.ticktok.server.clock.actions;
 
 import io.ticktok.server.clock.Clock;
-import io.ticktok.server.clock.repository.ClocksFinder;
+import io.ticktok.server.clock.repository.RepositoryClocksFinder;
 import io.ticktok.server.clock.repository.ClocksRepository;
 
 public abstract class RepositoryClockAction implements ClockAction {
@@ -14,7 +14,7 @@ public abstract class RepositoryClockAction implements ClockAction {
 
     @Override
     public void run(String id) {
-        Clock clock = new ClocksFinder(clocksRepository).findById(id);
+        Clock clock = new RepositoryClocksFinder(clocksRepository).findById(id);
         runOnClock(clock);
     }
 
---
+++ src/test/java/test/io/ticktok/server/clock/CachedClocksFinderTest.java
@@ -0,0 +1,50 @@
+package test.io.ticktok.server.clock;
+
+import com.google.common.collect.ImmutableMap;
+import io.ticktok.server.clock.CachedClocksFinder;
+import io.ticktok.server.clock.Clock;
+import io.ticktok.server.clock.ClocksFinder;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class CachedClocksFinderTest {
+
+    static final List<Clock> CLOCKS = Arrays.asList(new Clock("lala", "every.12.seconds"));
+    static final ImmutableMap<String, String> PARAMS = ImmutableMap.of("name", "lala");
+    private static final int TTL = 1;
+
+    final ClocksFinder clocksFinder = mock(ClocksFinder.class);
+    final CachedClocksFinder cachedClocksFinder = new CachedClocksFinder(clocksFinder, TTL);
+
+    @Test
+    void delegateParamsToFinder() {
+        cachedClocksFinder.findBy(PARAMS);
+        verify(clocksFinder).findBy(PARAMS);
+    }
+
+    @Test
+    void delegateResultFromFinder() {
+        when(clocksFinder.findBy(PARAMS)).thenReturn(CLOCKS);
+        assertThat(cachedClocksFinder.findBy(PARAMS)).isEqualTo(CLOCKS);
+    }
+
+    @Test
+    void shouldNotDelegateASecondTimeOnConsecutiveCalls() {
+        cachedClocksFinder.findBy(PARAMS);
+        cachedClocksFinder.findBy(PARAMS);
+        verify(clocksFinder, times(1)).findBy(PARAMS);
+    }
+
+    @Test
+    void shouldExpireCache() throws InterruptedException {
+        cachedClocksFinder.findBy(PARAMS);
+        Thread.sleep(TTL * 1000 + 100);
+        cachedClocksFinder.findBy(PARAMS);
+        verify(clocksFinder, times(2)).findBy(PARAMS);
+    }
+}
