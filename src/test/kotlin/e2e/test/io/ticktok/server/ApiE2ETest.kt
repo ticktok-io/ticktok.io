b367fc79a71fc560866f94f6302979d0614dd42b
Eli Segal <eli.segal@M-csr-84.lan>
2020-11-27 17:02:29 +0200

    wip

--- src/test/kotlin/e2e/test/io/ticktok/server/ApiE2ETest.kt
+++ src/test/kotlin/e2e/test/io/ticktok/server/ApiE2ETest.kt
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import java.lang.Thread.sleep
 
-@Tag("rabbit-broker")
 class ApiE2ETest : CommonAppE2ETest() {
 
     override fun appProfile(): String {
